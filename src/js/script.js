2ebe9da80b4bbcbf9ddea94261d6fcf08ba26bdc
Adnan Damadzic <adnan.damadzic@gmail.com>
2017-12-01 15:49:18 +0430

    ajax

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,11 @@
 $(document).ready(function(){
 
- /* function getData() {
+  function getData() {
     return $.getJSON('data.json');
-  }*/
+  }
 
-  //$.when(getData()).then(function (data) {
-    var allImages = [
-  "img1",
-  "img2",
-  "img3",
-  "img4",
-  "img5",
-  "img6",
-  "img7",
-  "img8",
-  "img9",
-  "img10",
-  "img11",
-  "img12",
-  "img13",
-  "img14",
-  "img15",
-  "img16",
-  "img17",
-  "img18",
-  "img19",
-  "img20"
-];
+  $.when(getData()).then(function (data) {
+    var allImages = data;
     var boxes;
     var images = [];
     var finalImages = [];
@@ -218,5 +197,5 @@ $(document).ready(function(){
       checkResult();
     });
 
-  //});
+  });
 })
